248cb4d28ebadf3b2fbb93bbb5db3ce25232e5ab
mai <maisishuai@outlook.com>
2022-11-26 14:20:16 +0800

    application

--- springboot-crud/src/test/java/com/mai/springbootlibraryms/SpringBootCRUDApplicationTests.java
+++ springboot-crud/src/test/java/com/mai/springbootlibraryms/SpringBootCRUDApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootLibrarymsApplicationTests {
+class SpringBootCRUDApplicationTests {
 
     @Test
     void contextLoads() {
